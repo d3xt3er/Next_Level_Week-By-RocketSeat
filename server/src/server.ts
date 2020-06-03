c26759b0cff34d7cb0f6e37b1aa15b2e4c3f2600
d3xt3er <48564999+d3xt3er@users.noreply.github.com>
2020-06-02 21:48:44 -0300

    Atualizações...
    Back-End completo

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,13 @@
 import express from 'express';
+import routes from './routes';
+import cors from 'cors';
+import path from 'path';
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-app.get('/users', (req, res) => {
-    console.log('Listagem de usuarios');
-
-    res.json([
-        'Hello NLW',
-        'Hello Coder!'
-    ]);
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
